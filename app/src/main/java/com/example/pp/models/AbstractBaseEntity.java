bd1c016c2d676b3c5f8ca1019033c7d192f665f0
Yevgen <notky@urk.net>
2019-12-02 18:36:25 +0200

    add Setting entity

---
+++ app/src/main/java/com/example/pp/models/AbstractBaseEntity.java
@@ -0,0 +1,20 @@
+package com.example.pp.models;
+
+public class AbstractBaseEntity {
+    private Integer id;
+
+    public AbstractBaseEntity() {
+    }
+
+    public AbstractBaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}

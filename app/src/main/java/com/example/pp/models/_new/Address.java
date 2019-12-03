30b09c70727ab1303a009d2be4b3c6d6849a3c2d
Notkyn <notky@ukr.net>
2019-12-03 15:38:47 +0200

    add Address model

---
+++ app/src/main/java/com/example/pp/models/_new/Address.java
@@ -0,0 +1,65 @@
+package com.example.pp.models._new;
+
+import java.util.Objects;
+
+public class Address {
+    private String name;
+    private int index;
+    private int idShop;
+
+    public Address() {
+    }
+
+    public Address(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getIdShop() {
+        return idShop;
+    }
+
+    public void setIdShop(int idShop) {
+        this.idShop = idShop;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return index == address.index &&
+                idShop == address.idShop &&
+                Objects.equals(name, address.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, index, idShop);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "name='" + name + '\'' +
+                ", index=" + index +
+                ", idShop=" + idShop +
+                '}';
+    }
+}
